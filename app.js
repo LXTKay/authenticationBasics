9883bae86b7f758771c705fa5b1a2d33de54645a
lxtk <thealextk@gmail.com>
2024-06-18 14:43:59 +0200

    buggy

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
 
 const mongoDb = process.env.DBURL;
 mongoose.connect(mongoDb);
@@ -30,17 +31,73 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, false, { message: "Incorrect password" });
+      };
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get("/sign-up", (req, res)=> res.render("signupForm"));
 
 app.post("/sign-up", asyncHandler(async function(req, res, next){
-  const user = new User({
-  username: req.body.username,
-  password: req.body.password
+  await bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword
+      });
+      const result = await user.save();
+      res.redirect("/");
   });
-  const result = await user.save();
-  res.redirect("/");
 }));
 
-app.get("/", (req, res) => res.render("index"));
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
